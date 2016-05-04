6faf9ecf2dc9f2ce80fc94a388eb2661291783f5
alanyeh <alanyeh20001@hotmail.com>
2016-05-04 10:28:12 +0800

    complete capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,14 @@
 # config valid only for current version of Capistrano
-lock '3.5.0'
+lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'training'
+set :repo_url, 'git@github.com:alanyeh20001/training.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/home/apps/training'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -23,11 +23,14 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for :pty is false
 # set :pty, true
 
+# Default value for :log_level is :debug
+set :log_level, :debug
+
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, %w(config/database.yml config/secrets.yml)
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('bin','log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -35,15 +38,36 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+namespace :passenger do
+  task :restart do
+    on roles(:web) do 
+      within release_path do
+        execute :touch, 'tmp/restart.txt'
+      end
     end
   end
-
 end
+
+after "deploy:publishing", "deploy:restart"
+after "deploy:restart", "passenger:restart"
+
+
+# namespace :deploy do
+
+#   task :restart do
+#     on roles(:web), in: :sequence, wait: 5 do 
+#       execute :mkdir, '-p', "#{ release_path }/tmp"
+#       execute :touch, release_path.join('tmp/restart.txt')
+#     end
+#   end 
+
+#   after :restart, :clear_cache do
+#     on roles(:web), in: :groups, limit: 3, wait: 10 do
+#       # Here we can do anything such as:
+#       # within release_path do
+#       #   execute :rake, 'cache:clear'
+#       # end
+#     end
+#   end
+
+# end
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,7 +4,7 @@
 # You can define all roles on a single server, or split them:
 
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
-# server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
+# server '130.211.252.152', user: 'apps', roles: %w{app web}
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
 
@@ -17,9 +17,9 @@
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+role :app, %w{130.211.252.152}
+role :web, %w{130.211.252.152}
+role :db,  %w{130.211.252.152}
 
 
 
@@ -41,11 +41,10 @@
 #
 # Global options
 # --------------
-#  set :ssh_options, {
-#    keys: %w(/home/rlisowski/.ssh/id_rsa),
-#    forward_agent: false,
-#    auth_methods: %w(password)
-#  }
+set :ssh_options, {
+  user: "apps",
+  forward_agent: true
+}
 #
 # The server-based syntax can be used to override options:
 # ------------------------------------
