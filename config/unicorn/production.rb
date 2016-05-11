c9e5db310c5582ff1c13b51c193ece1813dc3007
alanyeh <alanyeh20001@hotmail.com>
2016-05-11 13:16:52 +0800

    complete unicorn preload_app configuration

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -6,3 +6,28 @@ pid "/home/apps/training/shared/tmp/pids/unicorn.pid"
 
 stderr_path "log/unicorn.stderr.log"
 stdout_path "log/unicorn.stdout.log"
+
+preload_app true
+GC.respond_to?(:copy_on_write_friendly=) and
+  GC.copy_on_write_friendly = true
+
+check_client_connection false
+
+before_fork do |server, worker|
+  defined?(ActiveRecord::Base) and 
+    ActiveRecord::Base.connection.disconnect!
+
+  old_pid = "#{server.config[:pid]}.oldbin"
+  if old_pid != server.pid
+    begin
+      sig = (worker.nr + 1) >= server.worker_processes ? :QUIT : :TTOU
+      Process.kill(sig, File.read(old_pid).to_i)
+    rescue Errno::ENOENT, Errno::ESRCH
+    end
+  end
+end
+
+after_fork do |server, worker|
+  defined?(ActiveRecord::Base) and
+    ActiveRecord::Base.establish_connection
+end
